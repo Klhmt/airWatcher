73c00dd4b4cd2d5030bc87ec975af9b77360af0b
lMarnas <leo.marnas@gmail.com>
2025-05-16 14:58:52 +0200

    Ajout Méthodes classe Sensor

--- Sensor.cpp
+++ Sensor.cpp
@@ -1,5 +1,12 @@
 #include "Sensor.h"
 
+// Constructeur par défaut
+Sensor::Sensor()
+{
+ 
+}
+
+// Constructeur avec paramètres
 Sensor::Sensor(string id, float lat, float lon, bool defectueux, PrivateOwner* prop)
 {
     sensorId = id;
@@ -9,12 +16,68 @@ Sensor::Sensor(string id, float lat, float lon, bool defectueux, PrivateOwner* p
     proprietaire = prop;
 }
 
+// Destructeur
 Sensor::~Sensor()
 {
     delete proprietaire;
     proprietaire = nullptr;
 }
 
+// Getters
+string Sensor::getSensorId() const
+{
+    return sensorId;
+}
+
+float Sensor::getLatitude() const
+{
+    return latitude;
+}
+
+float Sensor::getLongitude() const
+{
+    return longitude;
+}
+
+bool Sensor::getEstDefectueux() const
+{
+    return estDefectueux;
+}
+
+PrivateOwner* Sensor::getProprietaire() const
+{
+    return proprietaire;
+}
+
+
+// Setters
+void Sensor::setSensorId(string id)
+{
+    sensorId = id;
+}
+
+void Sensor::setLatitude(float lat)
+{
+    latitude = lat;
+}
+
+void Sensor::setLongitude(float lon)
+{
+    longitude = lon;
+}
+
+void Sensor::setEstDefectueux(bool defectueux)
+{
+    estDefectueux = defectueux;
+}
+
+void Sensor::setProprietaire(PrivateOwner* owner)
+{
+    proprietaire = owner;
+}
+
+
+// Autres méthodes
 void Sensor::desactiverCapteur()
 {
     estDefectueux = true;
--- Sensor.h
+++ Sensor.h
@@ -10,15 +10,41 @@ class Sensor
 {
     public :
 
+    // Constructeurs
+    Sensor(); 
     Sensor(string id, float lat, float lon, bool defectueux, PrivateOwner* owner);
 
+    // Destructeur
     ~Sensor();
 
+    // Getters 
+    string getSensorId() const;
+
+    float getLatitude() const;
+
+    float getLongitude() const;
+
+    bool getEstDefectueux() const;
+
+    PrivateOwner* getProprietaire() const;
+
+    // Setters
+    void setSensorId(string id);
+
+    void setLatitude(float lat);
+
+    void setLongitude(float lon);
+
+    void setEstDefectueux(bool defectueux);
+
+    void setProprietaire(PrivateOwner* owner);
+
+    // Autres méthodes 
     void desactiverCapteur();
 
 
     private :
-
+    // Attributs privés
     string sensorId;
     float latitude;
     float longitude;
